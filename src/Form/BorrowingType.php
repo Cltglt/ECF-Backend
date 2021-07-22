b2ff5f2ba5e9156c8bf715cfe36be62de3bb7b7a
Clem <cmgallet51@gmail.com>
2021-07-22 16:02:10 +0200

    make crud for borrowing

---
+++ src/Form/BorrowingType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Borrowing;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BorrowingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date_borrowing')
+            ->add('date_return')
+            ->add('book')
+            ->add('borrower')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Borrowing::class,
+        ]);
+    }
+}
